1d66be4eb314fad548317d871d550557b85f3115
jowenshaw <jowenshaw@gmail.com>
2021-03-17 11:31:20 +0800

    init router bridge signer

--- tokens/router/bridge.go
+++ tokens/router/bridge.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"errors"
 	"fmt"
 	"math/big"
 	"strings"
@@ -58,12 +59,12 @@ func InitRouterBridges(isServer bool) {
 		bridge := NewCrossChainBridge()
 		bridge.initGatewayConfig(chainID)
 		bridge.initChainConfig(chainID)
+		bridge.initSigner(chainID)
 
 		for _, tokenID := range tokenIDs {
 			bridge.initTokenConfig(tokenID, chainID)
 		}
 
-		bridge.Signer = types.MakeSigner("EIP155", chainID)
 		RouterBridges[chainID.String()] = bridge
 	}
 	printPeerTokens()
@@ -94,7 +95,7 @@ func (b *Bridge) initGatewayConfig(chainID *big.Int) {
 	log.Infof(">>> [%5v] init gateway config success", chainID)
 }
 
-func (b *Bridge) initChainConfig(chainID *big.Int) bool {
+func (b *Bridge) initChainConfig(chainID *big.Int) {
 	chainCfg, err := GetChainConfig(chainID)
 	if err != nil {
 		log.Fatal("get chain config failed", "chainID", chainID, "err", err)
@@ -107,7 +108,17 @@ func (b *Bridge) initChainConfig(chainID *big.Int) bool {
 	}
 	b.SetChainConfig(chainCfg)
 	log.Infof(">>> [%5v] init chain config success", chainID)
-	return true
+}
+
+func (b *Bridge) initSigner(chainID *big.Int) {
+	signerChainID, err := b.GetSignerChainID()
+	if err != nil {
+		log.Fatal("get signer chain ID failed", "chainID", chainID, "err", err)
+	}
+	if chainID.Cmp(signerChainID) != 0 {
+		log.Fatal("chain ID mismatch", "inconfig", chainID, "inbridge", signerChainID)
+	}
+	b.Signer = types.MakeSigner("EIP155", signerChainID) // init Signer
 }
 
 func (b *Bridge) initTokenConfig(tokenID string, chainID *big.Int) {
@@ -157,4 +168,45 @@ func printPeerTokens() {
 		}
 	}
 	log.Info(">>> end print all peer tokens")
+}
+
+// GetSignerChainID default way to get signer chain id
+// use chain ID first, if missing then use network ID instead.
+// normally this way works, but sometimes it failed (eg. ETC),
+// then we should overwrite this function
+// NOTE: call after chain config setted
+func (b *Bridge) GetSignerChainID() (*big.Int, error) {
+	switch strings.ToUpper(b.ChainConfig.BlockChain) {
+	default:
+		chainID, err := b.ChainID()
+		if err != nil {
+			return nil, err
+		}
+		if chainID.Sign() != 0 {
+			return chainID, nil
+		}
+		return b.NetworkID()
+	case "ETHCLASSIC":
+		return b.getETCSignerChainID()
+	}
+}
+
+func (b *Bridge) getETCSignerChainID() (*big.Int, error) {
+	networkID, err := b.NetworkID()
+	if err != nil {
+		return nil, err
+	}
+	var chainID uint64
+	switch networkID.Uint64() {
+	case 1:
+		chainID = 61
+	case 6:
+		chainID = 6
+	case 7:
+		chainID = 63
+	default:
+		log.Warnf("unsupported etc network id '%v'", networkID)
+		return nil, errors.New("unsupported etc network id")
+	}
+	return new(big.Int).SetUint64(chainID), nil
 }
