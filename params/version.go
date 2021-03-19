73c7e3f6d55fba96a2654c859365d053c90fde9d
jowenshaw <jowenshaw@gmail.com>
2021-03-19 13:18:00 +0800

    modify version to 2.0.0

--- params/version.go
+++ params/version.go
@@ -6,9 +6,9 @@ import (
 
 // version parts
 const (
-	VersionMajor = 0  // Major version component of the current release
-	VersionMinor = 3  // Minor version component of the current release
-	VersionPatch = 5  // Patch version component of the current release
+	VersionMajor = 2  // Major version component of the current release
+	VersionMinor = 0  // Minor version component of the current release
+	VersionPatch = 0  // Patch version component of the current release
 	VersionMeta  = "" // Version metadata to append to the version string
 )
 
